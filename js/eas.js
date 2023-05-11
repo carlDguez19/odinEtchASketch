77e7ec03e1c8ab2240932b75a897eee9c5e91474
carlDguez19 <carldominguez19@gmail.com>
2023-05-11 17:07:32 -0600

    have a basic skeleton working: 1. have the basic aesthetic of the etch-A-Sketch 2. grid displays in the correct spot 3. a simple button adds the grid into the grid area    ~BUGS~ 1. there is a black border at the top of the grid area that needs to be removed   2. button can be clicked multiple times adding more and more grids(need to clear grid area before adding anything)

---
+++ js/eas.js
@@ -0,0 +1,20 @@
+const button = document.querySelector('.buttonA');
+
+button.addEventListener('click', createGrid);
+
+function createGrid(){
+    let rowHeight = 25/50;
+    for(let i = 0; i < 50; i++){
+        console.log("here");
+        const row = document.createElement('div');
+        row.style.height = rowHeight + 'em';
+        row.classList.add('row');
+        for(let j = 0; j < 50; j++){
+            console.log(j);
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            row.appendChild(cell);
+        }
+        document.querySelector('.gridContainer').appendChild(row);
+    }
+}
